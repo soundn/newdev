ae07785fc5654af4a4d08460c68cf069dd9e41e8
soundn <soundnwankwo@gmail.com>
2024-05-14 11:20:23 +0100

    added more commands

--- shellcommands.sh
+++ shellcommands.sh
@@ -1,3 +1,5 @@
 ls -la # to list contents of a directory
 pwd # to print working directory
 cat # to display the content of a file on the terminal
+touch # to create file 
+grep # to filter selection
